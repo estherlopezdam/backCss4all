2baf8a1ff0c9fac82a19397995c6b8dca4f55768
Esther Lopez Perez <82450251+estherlopezdam@users.noreply.github.com>
2025-05-05 18:38:49 +0200

    primera version viable

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -6,5 +6,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'static', // o 'server' si vas a usar adaptador Node
+  build: {
+    format: 'directory', // recomendado para producción
+  },
 
 });
