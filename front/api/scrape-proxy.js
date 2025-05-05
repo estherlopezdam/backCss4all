a2a2cb0dccb205ea716ea6c8e85abce4072e7913
Esther Lopez Perez <82450251+estherlopezdam@users.noreply.github.com>
2025-05-05 19:59:36 +0200

    primera version viable

--- front/api/scrape-proxy.js
+++ front/api/scrape-proxy.js
@@ -1,27 +1,28 @@
 export default async function handler(req, res) {
-    const API_URL = import.meta.env.PUBLIC_API_URL;
+    const API_URL = process.env.PUBLIC_API_URL;
+  
     if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Solo se permiten peticiones POST' });
-      }
-    
-      try {
-        const response = await fetch('http://217.154.18.122:3001/scrape', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(req.body),
-        });
-    
-        const contentType = response.headers.get('content-type') || '';
-    
-        if (contentType.includes('application/json')) {
-          const data = await response.json();
-          res.status(response.status).json(data);
-        } else {
-          const text = await response.text();
-          res.status(500).json({ error: 'La API no devolvió JSON', detalle: text });
-        }
-      } catch (error) {
-        console.error('Error en el proxy:', error);
-        res.status(500).json({ error: 'Error en el proxy', detalle: error.message });
-      }
+      return res.status(405).json({ error: 'Solo se permiten peticiones POST' });
     }
+  
+    try {
+      const response = await fetch(`${API_URL}:3001/scrape`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(req.body),
+      });
+  
+      const contentType = response.headers.get('content-type') || '';
+  
+      if (contentType.includes('application/json')) {
+        const data = await response.json();
+        res.status(response.status).json(data);
+      } else {
+        const text = await response.text();
+        res.status(500).json({ error: 'La API no devolvió JSON', detalle: text });
+      }
+    } catch (error) {
+      console.error('Error en el proxy:', error);
+      res.status(500).json({ error: 'Error en el proxy', detalle: error.message });
+    }
+  }
